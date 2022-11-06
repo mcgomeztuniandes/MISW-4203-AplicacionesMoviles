30224c314cadd72f9095cfd6e56eb8f6b43ee314
Milton Gómez <mc.gomezt1@uniandes.edu.co>
2022-11-06 12:01:37 -0500

    Se genera nuevo apk para validar issue de duplicidad en instlacion en dispositivos fisicos

--- app/src/main/java/com/example/vinilosgrupo15/view/fragments/MusicianListFragment.kt
+++ app/src/main/java/com/example/vinilosgrupo15/view/fragments/MusicianListFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.core.view.isInvisible
-import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
