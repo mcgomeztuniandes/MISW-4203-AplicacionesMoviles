47b0cb454104bd69c83b229d3d970347b45ad494
Nelson Alonso Barreto Pastrana <nbarreto@uniandes.edu.co>
2022-11-19 13:32:51 -0500

    Pruebas e2e sprint 2

--- app/src/androidTest/java/e2e/TestAlbums.java
+++ app/src/androidTest/java/e2e/TestAlbums.java
@@ -2,12 +2,16 @@ package e2e;
 
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.action.ViewActions.clearText;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isEnabled;
+
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withSpinnerText;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
@@ -17,23 +21,26 @@ import static org.hamcrest.CoreMatchers.startsWith;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.hasToString;
 
-import androidx.recyclerview.widget.RecyclerView;
+import android.view.View;
+
+import androidx.test.espresso.UiController;
 import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
 import com.example.vinilosgrupo15.R;
 import com.example.vinilosgrupo15.view.LoginActivity;
-import com.example.vinilosgrupo15.view.MainActivity;
 
-import org.hamcrest.CustomMatcher;
 import org.hamcrest.Matcher;
 import org.junit.Rule;
 import org.junit.Test;
 
 public class TestAlbums {
+    private static final int ITEM_BELOW_THE_FOLD = 0;
     @Rule
     public ActivityScenarioRule<LoginActivity> activityRule =
-            new ActivityScenarioRule<LoginActivity>(LoginActivity.class);
+            new ActivityScenarioRule<>(LoginActivity.class);
 
     @Test
     public void testNavegarListaAlbums() throws InterruptedException {
@@ -60,6 +67,87 @@ public class TestAlbums {
         onView(withId(R.id.homeBtn)).perform(click());
         Thread.sleep(1000);
 
+    }
+
+    @Test
+    public void testNavegarAlbumsInformacion() throws InterruptedException {
+        onView(withText("Vinilos Grupo 15")).check(matches(isDisplayed()));
+
+        /* Autenticar usuario guest */
+        onView(withId(R.id.editTextTextPersonName)).perform(clearText(), typeText("Invitado"), closeSoftKeyboard());
+        onView(withId(R.id.editTextTextPassword)).perform(clearText(), typeText("Invitado"), closeSoftKeyboard());
+        onView(withId(R.id.rolList)).perform(click());
+        onData(hasToString(startsWith("Guest"))).perform(click());
+        onView(withId(R.id.rolList)).check(matches(withSpinnerText(containsString("Guest"))));
+        onView(withId(R.id.login)).perform(click());
+
+        /* Ir a Albums */
+        onView(withId(R.id.buttonAlbums)).check(matches(isDisplayed()));
+        onView(withId(R.id.buttonAlbums)).perform(click());
+
+        Thread.sleep(1000);
+        /*  Valida imagenes en Recyclerview */
+        onView(allOf(withId(R.id.img_album), isDisplayed()));
+
+        /* Ingresar al primer elemento del recyclerview */
+        Thread.sleep(1000);
+
+        onView(ViewMatchers.withId(R.id.recyclerview))
+                .perform(
+                        RecyclerViewActions.actionOnItemAtPosition(
+                                ITEM_BELOW_THE_FOLD,
+                                click()
+                        )
+                );
+
+        /* Ingresar a tracks */
+        Thread.sleep(1000);
+        onView(withId(R.id.btnTracks)).check(matches(isDisplayed()));
+        onView(withId(R.id.btnTracks)).perform(forceClick());
+
+        Thread.sleep(1000);
+        pressBack();
+
+        /* Ingresar a Comments */
+        Thread.sleep(1000);
+        onView(withId(R.id.btnComments)).check(matches(isDisplayed()));
+        onView(withId(R.id.btnComments)).perform(forceClick());
+
+        Thread.sleep(1000);
+        pressBack();
+
+        /* Ingresar a Comments */
+        Thread.sleep(1000);
+        onView(withId(R.id.btnPerformers)).check(matches(isDisplayed()));
+        onView(withId(R.id.btnPerformers)).perform(forceClick());
+
+        Thread.sleep(1000);
+        pressBack();
+        Thread.sleep(1000);
+
+        /* Volver al home */
+        onView(withId(R.id.homeBtn)).check(matches(isDisplayed()));
+        onView(withId(R.id.homeBtn)).perform(click());
+        Thread.sleep(1000);
+
+    }
+
+
+    public static ViewAction forceClick() {
+        return new ViewAction() {
+            @Override public Matcher<View> getConstraints() {
+                return allOf(isClickable(), isEnabled(), isDisplayed());
+            }
+
+            @Override public String getDescription() {
+                return "force click";
+            }
+
+            @Override public void perform(UiController uiController, View view) {
+                view.performClick();
+                uiController.loopMainThreadUntilIdle();
+            }
+        };
     }
 
 
--- app/src/androidTest/java/e2e/TestPerformers.java
+++ app/src/androidTest/java/e2e/TestPerformers.java
@@ -2,12 +2,15 @@ package e2e;
 
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.action.ViewActions.clearText;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isEnabled;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withSpinnerText;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
@@ -16,10 +19,17 @@ import static org.hamcrest.CoreMatchers.startsWith;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.hasToString;
 
+import android.view.View;
+
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.filters.LargeTest;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.hamcrest.Matcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -32,6 +42,7 @@ import com.example.vinilosgrupo15.view.MainActivity;
 @LargeTest
 
 public class TestPerformers {
+    private static final int ITEM_BELOW_THE_FOLD = 0;
     @Rule
     public ActivityScenarioRule<LoginActivity> activityRule =
             new ActivityScenarioRule<LoginActivity>(LoginActivity.class);
@@ -65,6 +76,48 @@ public class TestPerformers {
     }
 
     @Test
+    public void testNavegarPerformersInformacion() throws InterruptedException {
+        onView(withText("Vinilos Grupo 15")).check(matches(isDisplayed()));
+
+        /* Autenticar usuario guest */
+        onView(withId(R.id.editTextTextPersonName)).perform(clearText(), typeText("Invitado"), closeSoftKeyboard());
+        onView(withId(R.id.editTextTextPassword)).perform(clearText(), typeText("Invitado"), closeSoftKeyboard());
+        onView(withId(R.id.rolList)).perform(click());
+        onData(hasToString(startsWith("Guest"))).perform(click());
+        onView(withId(R.id.rolList)).check(matches(withSpinnerText(containsString("Guest"))));
+        onView(withId(R.id.login)).perform(click());
+
+        /* ir a interprete */
+        onView(withId(R.id.buttonPerformers)).check(matches(isDisplayed()));
+        onView(withId(R.id.buttonPerformers)).perform(click());
+        Thread.sleep(2000);
+
+        /* Valida que exista RecyleView interprete */
+        onView(withId(R.id.recyclerview)).check(matches(isDisplayed()));
+        onView(allOf(withId(R.id.img_character), isDisplayed()));
+
+        Thread.sleep(1000);
+
+        /* Ver informacion interprete */
+        onView(ViewMatchers.withId(R.id.recyclerview))
+                .perform(
+                        RecyclerViewActions.actionOnItemAtPosition(
+                                ITEM_BELOW_THE_FOLD,
+                                click()
+                        )
+                );
+
+        Thread.sleep(1000);
+        pressBack();
+
+        /* Volver al home */
+        onView(withId(R.id.homeBtn)).check(matches(isDisplayed()));
+        onView(withId(R.id.homeBtn)).perform(click());
+        Thread.sleep(2000);
+
+    }
+
+    @Test
     public void testNavegarListaBands() throws InterruptedException {
         onView(withText("Vinilos Grupo 15")).check(matches(isDisplayed()));
 
@@ -83,7 +136,55 @@ public class TestPerformers {
         /* Pasar a bandas */
         Thread.sleep(1000);
         onView(withId(R.id.btnBand)).check(matches(isDisplayed()));
-        onView(withId(R.id.btnBand)).perform(click());
+        onView(withId(R.id.btnBand)).perform(forceClick());
+
+
+        /* Valida que exista RecyleView interprete */
+        Thread.sleep(1000);
+        onView(withId(R.id.recyclerview)).check(matches(isDisplayed()));
+
+        Thread.sleep(1000);
+
+        /* Ver informacion banda */
+        onView(ViewMatchers.withId(R.id.recyclerview))
+                .perform(
+                        RecyclerViewActions.actionOnItemAtPosition(
+                                ITEM_BELOW_THE_FOLD,
+                                click()
+                        )
+                );
+
+        Thread.sleep(1000);
+        pressBack();
+        Thread.sleep(1000);
+
+        /* Volver al home */
+        onView(withId(R.id.homeBtn)).check(matches(isDisplayed()));
+        onView(withId(R.id.homeBtn)).perform(click());
+        Thread.sleep(2000);
+
+    }
+
+    @Test
+    public void testNavegarBandsInformacion() throws InterruptedException {
+        onView(withText("Vinilos Grupo 15")).check(matches(isDisplayed()));
+
+        /* Autenticar usuario guest */
+        onView(withId(R.id.editTextTextPersonName)).perform(clearText(), typeText("Invitado"), closeSoftKeyboard());
+        onView(withId(R.id.editTextTextPassword)).perform(clearText(), typeText("Invitado"), closeSoftKeyboard());
+        onView(withId(R.id.rolList)).perform(click());
+        onData(hasToString(startsWith("Guest"))).perform(click());
+        onView(withId(R.id.rolList)).check(matches(withSpinnerText(containsString("Guest"))));
+        onView(withId(R.id.login)).perform(click());
+
+        /* ir a interprete */
+        onView(withId(R.id.buttonPerformers)).check(matches(isDisplayed()));
+        onView(withId(R.id.buttonPerformers)).perform(click());
+
+        /* Pasar a bandas */
+        Thread.sleep(1000);
+        onView(withId(R.id.btnBand)).check(matches(isDisplayed()));
+        onView(withId(R.id.btnBand)).perform(forceClick());
 
 
         /* Valida que exista RecyleView interprete */
@@ -92,9 +193,27 @@ public class TestPerformers {
 
         /* Volver al home */
         onView(withId(R.id.homeBtn)).check(matches(isDisplayed()));
-        onView(withId(R.id.homeBtn)).perform(click());
+        onView(withId(R.id.homeBtn)).perform(forceClick());
         Thread.sleep(2000);
 
+    }
+
+
+    public static ViewAction forceClick() {
+        return new ViewAction() {
+            @Override public Matcher<View> getConstraints() {
+                return allOf(isClickable(), isEnabled(), isDisplayed());
+            }
+
+            @Override public String getDescription() {
+                return "force click";
+            }
+
+            @Override public void perform(UiController uiController, View view) {
+                view.performClick();
+                uiController.loopMainThreadUntilIdle();
+            }
+        };
     }
 
 
