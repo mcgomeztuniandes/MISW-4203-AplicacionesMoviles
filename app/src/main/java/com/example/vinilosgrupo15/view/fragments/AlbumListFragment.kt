2579d010e40f712a64ecc13c4ad058e5e9915eac
dcastrog2uniandes <d.castrog2@uniandes.edu.co>
2022-11-24 01:05:25 -0500

    Se agregó botón flotante en el listado de albumes para redirigir al formulario de creación.

--- app/src/main/java/com/example/vinilosgrupo15/view/fragments/AlbumListFragment.kt
+++ app/src/main/java/com/example/vinilosgrupo15/view/fragments/AlbumListFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.core.view.isInvisible
 import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
@@ -15,6 +16,7 @@ import com.example.vinilosgrupo15.databinding.FragmentAlbumListBinding
 import com.example.vinilosgrupo15.model.AlbumResponseDataModel
 import com.example.vinilosgrupo15.view.adapter.ItemsAlbumAdapter
 import com.example.vinilosgrupo15.viewmodels.AlbumViewModel
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class AlbumListFragment: Fragment(), ClickAlbumListener {
@@ -36,6 +38,15 @@ class AlbumListFragment: Fragment(), ClickAlbumListener {
     ): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_album_list, container, false)
         binding.viewModel = viewAlbumModel
+        var btnCreateAlbum: FloatingActionButton = binding!!.btnCreate
+
+        btnCreateAlbum.setOnClickListener{
+            activity?.supportFragmentManager
+                ?.beginTransaction()
+                ?.replace(R.id.container, AlbumCreateFragment())
+                ?.addToBackStack(null)
+                ?.commit()
+        }
 
         return binding.root
     }
