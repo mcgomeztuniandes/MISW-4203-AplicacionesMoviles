46e4c2359bd00d2328c6e192e7b26f087bba8e35
Milton Gómez <mc.gomezt1@uniandes.edu.co>
2022-11-25 07:59:44 -0500

    Se finaliza las pruebas se completa codigo para integracion

--- app/src/main/java/com/example/vinilosgrupo15/view/fragments/AlbumDetailInfoFragment.kt
+++ app/src/main/java/com/example/vinilosgrupo15/view/fragments/AlbumDetailInfoFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.EditText
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
